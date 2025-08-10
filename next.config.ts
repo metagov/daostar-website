0633c1719328f1f82e252bd2612670118c3b985a
jolow99 <jolow999@gmail.com>
2025-08-10 16:19:28 +0800

    Rename reports to research. Setup redirect from reports to research to preserve old links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/reports/:path*',
+        destination: '/research/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
